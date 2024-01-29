f7b3c46c9141e4cee0ebe8594ff53e5ad7107174
Jonathan Van Schenck <jvschenck@novadynamics.com>
2024-01-29 08:20:50 -0800

    Pack 'SLITAN_PUBLIC_' env into 'process.env' wrapper

--- cli/build.js
+++ cli/build.js
@@ -116,7 +116,7 @@ const { f } = require("yaclc");
                 ".html": "text",
             },
             define: {
-                ...env.get() 
+                "process.env": env.toJSON(),
             },
             plugins
         }); 
